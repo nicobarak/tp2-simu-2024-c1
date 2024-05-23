70d1e884bb610b1c1fcfba06ea588e20bd558aaf
nicobarak <baraknicolas@gmail.com>
2024-05-23 12:16:55 -0300

    Nicolás Barak

--- data/movies.js
+++ data/movies.js
@@ -2,7 +2,8 @@ import getConnection from "./conn.js";
 import { ObjectId } from "mongodb";
 const DATABASE = "sample_mflix";
 const MOVIES = "movies";
-
+const USERS = "users";
+const COMMENTS = "comments";
 
 async function getAllMovies(pageSize, page) {
   const connectiondb = await getConnection();
@@ -14,6 +15,48 @@ async function getAllMovies(pageSize, page) {
     .skip(pageSize * page)
     .toArray();
   return movies;
+}
+
+async function getCommentsByUserId(userId) {
+  const connectiondb = await getConnection();
+
+  const user = await connectiondb
+    .db(DATABASE)
+    .collection(USERS)
+    .findOne({ _id: new ObjectId(userId) });
+
+  const comments = await connectiondb
+    .db(DATABASE)
+    .collection(COMMENTS)
+    .find({ email: user.email })
+    .toArray();
+
+    const comment_movie_ids = comments.map(comment =>  comment.movie_id);
+
+    const movies = await connectiondb
+    .db(DATABASE)
+    .collection(MOVIES)
+    .find({})
+    .toArray();
+
+
+  //const userComments = comments.filter((comment) => comment.email == user.email);
+
+  const fullComments = comments.map((comment) => {
+    const movie = movies.find(movie => movie._id.toString() == comment.movie_id.toString());  
+    console.log(movie.poster)
+    return {
+      _id: comment._id,
+      name: comment.name,
+      email: comment.email,
+      text: comment.text,
+      date: comment.date,
+      title: movie?.title,
+      poster: movie?.poster,
+    };
+  });
+
+  return fullComments;
 }
 
 async function getWinnerMovies() {
@@ -55,10 +98,12 @@ async function getMoviesOrderedByFresh() {
   const movies = await connectiondb
     .db(DATABASE)
     .collection(MOVIES)
-    .find({ 'tomatoes.fresh': { $exists: true } })
+    .find({ "tomatoes.fresh": { $exists: true } })
     .toArray();
 
-  const orderedMovies = movies.sort((a, b) => b.tomatoes.fresh - a.tomatoes.fresh);
+  const orderedMovies = movies.sort(
+    (a, b) => b.tomatoes.fresh - a.tomatoes.fresh
+  );
   return orderedMovies;
 }
 
@@ -68,4 +113,5 @@ export {
   getWinnerMovies,
   getMoviesByLangauge,
   getMoviesOrderedByFresh,
+  getCommentsByUserId,
 };
--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllMovies, findMovie, getWinnerMovies, getMoviesByLangauge, getMoviesOrderedByFresh } from "../data/movies.js";
+import { getAllMovies, findMovie, getWinnerMovies, getMoviesByLangauge, getMoviesOrderedByFresh, getCommentsByUserId } from "../data/movies.js";
 
 const router = express.Router();
 
@@ -27,9 +27,16 @@ router.get("/language/:language", async (req, res) => {
   res.json(await getMoviesByLangauge(pageSize, page, req.params.language));
 });
 
+router.get("/comments/:userId", async (req, res) => {
+  const comments = await getCommentsByUserId(req.params.userId);
+  res.json(comments);
+});
+
 router.get("/:_id", async (req, res) => {
   const movie = await findMovie(req.params._id);
   res.json(movie);
 });
+
+
 
 export default router;
